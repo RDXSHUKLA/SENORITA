5e28e3a8aed487dc0f22da71e3898813303932dc
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-05-25 13:41:14 +0530

    Update admin.py

--- MukeshRobot/modules/admin.py
+++ MukeshRobot/modules/admin.py
@@ -971,7 +971,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "▪️ᴀᴅᴍɪɴ▪️"
+__mod_name__ = "ᴀᴅᴍɪɴ"
 __command_list__ = [
     "setdesc" "setsticker" "setgpic" "delgpic" "setgtitle" "adminlist",
     "admins",
