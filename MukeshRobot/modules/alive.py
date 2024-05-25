9b3f7d26c1811fa2afe1dfd533e7f25b19dd5fab
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-05-25 13:28:56 +0530

    Update alive.py

--- MukeshRobot/modules/alive.py
+++ MukeshRobot/modules/alive.py
@@ -11,34 +11,39 @@ from telethon import __version__ as tver
 from MukeshRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID,BOT_NAME,START_IMG
 
 MISHI = [
-    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
-    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
-    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
-    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
-    "https://graph.org/file/84e84ff778b045879d24f.jpg",
-    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
-    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
-    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
-    "https://graph.org/file/37248e7bdff70c662a702.jpg",
-    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
-    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
-    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
-    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
-    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
-    "https://graph.org/file/39d7277189360d2c85b62.jpg",
-    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
-    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
-    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+    "https://telegra.ph/file/81aa2cd542ffe2beca305.jpg",
+"https://telegra.ph/file/3fbc8004d3d46bf37ae8a.jpg",
+"https://telegra.ph/file/e18b44b1a24feadcc67ce.jpg",
+"https://telegra.ph/file/3ed786ad4494b5474594a.jpg",
+"https://telegra.ph/file/b96d9b78484bbb7e60b17.jpg",
+"https://telegra.ph/file/ea16aa7f31ddea6160c35.jpg",
+"https://telegra.ph/file/8a7bfd8be8dcc506b1ed8.jpg",
+"https://telegra.ph/file/e680fb684eda5320f139b.jpg",
+"https://telegra.ph/file/093ddc7abc382d7088bb2.jpg",
+"https://telegra.ph/file/cb601b52914fd7afc5e97.jpg",
+"https://telegra.ph/file/4fb9db7007e5bebdde78c.jpg",
+"https://telegra.ph/file/98ca2a134994684d979d4.jpg",
+"https://telegra.ph/file/473605b2bb418f986352c.jpg",
+"https://graph.org/file/8dcccaaa09c3ec38d9c75.jpg",
+"https://graph.org/file/74d2385efc329c13c11e9.jpg",
+"https://graph.org/file/d818146f35f6a439a7a7f.jpg",
+"https://graph.org/file/d1d68eaaa8aecc68f8387.jpg",
+"https://graph.org/file/257fe1ec8828b836c70f7.jpg",
+"https://graph.org/file/8b044edab3d3173544439.jpg",
+"https://graph.org/file/8ed87bfd7c0b3dbdd1bf5.jpg",
+"https://graph.org/file/1a33887db1a3b5dee1b0a.jpg",
+"https://graph.org/file/04ea07e42660988229834.jpg",
+"https://graph.org/file/abb6b4bb00e2751bc9f54.jpg",
 ]
 
 Mukesh = [
     [
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/SHIVANSH474"),
-        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
+        InlineKeyboardButton(text="▪️ᴜᴘᴅᴀᴛᴇ▪️", url=f"https://t.me/SHIVANSH474"),
+        InlineKeyboardButton(text="▪️ꜱᴜᴘᴘᴏʀᴛ▪️", url=f"https://t.me/{SUPPORT_CHAT}"),
     ],
     [
         InlineKeyboardButton(
-            text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ",
+            text="🔸ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ🔸",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         ),
     ],
@@ -55,7 +60,7 @@ async def restart(client, m: Message):
     await asyncio.sleep(0.1)
     await accha.edit("🧡")
     await asyncio.sleep(0.1)
-    await accha.edit("💚")
+    await accha.edit("💻")
 
     await accha.delete()
     await asyncio.sleep(0.3)
@@ -66,14 +71,14 @@ async def restart(client, m: Message):
     await asyncio.sleep(0.2)
     await m.reply_photo(
         random.choice(MISHI),
-        caption=f"""** ✦ ʜᴇʏ, ɪ ᴀᴍ [{BOT_NAME}](f"t.me/{BOT_USERNAME}") ✦**\n\n❍ **ʟɪʙʀᴀʀʏ ➛** `{lver}`\n❍ **ᴛᴇʟᴇᴛʜᴏɴ ➛** `{tver}`\n❍ **ᴘʏʀᴏɢʀᴀᴍ ➛** `{pver}`\n❍ **ᴘʏᴛʜᴏɴ ➛** `{pyver()}`\n\n❍ **ᴍᴀᴅᴇ ʙʏ ➛** [sʜɪᴠᴀɴsʜ-xᴅ](tg://user?id={OWNER_ID})""",
+        caption=f"""**ꕤ ʜᴇʏ, ɪ ᴀᴍ [{BOT_NAME}](f"t.me/{BOT_USERNAME}") ꕤ**\n\nꕤ **ʟɪʙʀᴀʀʏ ➛** `{lver}`\nꕤ **ᴛᴇʟᴇᴛʜᴏɴ ➛** `{tver}`\nꕤ **ᴘʏʀᴏɢʀᴀᴍ ➛** `{pver}`\nꕤ **ᴘʏᴛʜᴏɴ ➛** `{pyver()}`\n\nꕤ **ᴍᴀᴅᴇ ʙʏ ➛** [sʜɪᴠᴀɴsʜ-xᴅ](tg://user?id={OWNER_ID})""",
         reply_markup=InlineKeyboardMarkup(Mukesh),
     )
     
-__mod_name__ = "ᴀʟɪᴠᴇ"
+__mod_name__ = "▪️ᴀʟɪᴠᴇ▪️"
 __help__ = """
- ❍ /alive ➛ ᴄʜᴇᴄᴋ ʙᴏᴛ ᴀʟɪᴠᴇ sᴛᴀᴛᴜs.
- ❍ /ping ➛ ᴄʜᴋ ᴘɪɴɢ sᴛᴀᴛᴜs.
- ❍ /pingall ➛ ᴄʜᴋ ᴘɪɴɢ sᴛᴀᴛᴜs ᴏғ ᴀʟʟ ᴍᴏᴅᴜʟᴇs.
+ ❖ /alive ➛ ᴄʜᴇᴄᴋ ʙᴏᴛ ᴀʟɪᴠᴇ sᴛᴀᴛᴜs.
+ ❖ /ping ➛ ᴄʜᴋ ᴘɪɴɢ sᴛᴀᴛᴜs.
+ ❖ /pingall ➛ ᴄʜᴋ ᴘɪɴɢ sᴛᴀᴛᴜs ᴏғ ᴀʟʟ ᴍᴏᴅᴜʟᴇs.
  """
     
