57493d958baa5cf80756124f70ab455f836f9eea
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-05-25 13:43:10 +0530

    Update anime.py

--- MukeshRobot/modules/anime.py
+++ MukeshRobot/modules/anime.py
@@ -139,7 +139,7 @@ dispatcher.add_handler(QUOTE_CHANGE)
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 dispatcher.add_handler(QUOTES_HANDLER)
 
-__mod_name__ = "▪️ǫᴜᴏᴛᴇ▪️"
+__mod_name__ = "ǫᴜᴏᴛᴇ"
 __help__ = """
 ❖ /quote ➛ ᴡʀɪᴛᴇ ǫᴜᴏᴛᴇs
 
