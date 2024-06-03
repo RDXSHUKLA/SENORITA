8a66d29448ca24015174db57220d6c40545e3265
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-06-03 21:26:24 +0530

    Update memify.py

--- MukeshRobot/modules/memify.py
+++ MukeshRobot/modules/memify.py
@@ -6,38 +6,31 @@ from PIL import Image, ImageDraw, ImageFont
 from MukeshRobot import telethn as bot
 from MukeshRobot.events import register
 
-
 @register(pattern="^/mmf ?(.*)")
 async def handler(event):
-
     if event.fwd_from:
-
         return
 
     if not event.reply_to_msg_id:
-
-        await event.reply("❍ ᴘʀᴏᴠɪᴅᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴅʀᴀᴡ !")
+        await event.reply("Provide Some Text To Draw!")
 
         return
 
     reply_message = await event.get_reply_message()
 
     if not reply_message.media:
-
-        await event.reply("```ʀᴇᴘʟʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ/sᴛɪᴄᴋᴇʀ.```")
+        await event.reply("```Reply to a image/sticker.```")
 
         return
 
     file = await bot.download_media(reply_message)
 
-    msg = await event.reply("```❍ ᴍᴇᴍɪғʏɪɴɢ ᴛʜɪs ɪᴍᴀɢᴇ ! ```")
-
+    msg = await event.reply("```Memifying this image! ✊🏻 ```")
 
     text = str(event.pattern_match.group(1)).strip()
 
     if len(text) < 1:
-
-        return await msg.reply("❍ ʏᴏᴜ ᴍɪɢʜᴛ ᴡᴀɴᴛ ᴛᴏ ᴛʀʏ `/mmf ᴛᴇxᴛ`")
+        return await msg.reply("You might want to try `/mmf text`")
 
     meme = await drawText(file, text)
 
@@ -49,7 +42,6 @@ async def handler(event):
 
 
 async def drawText(image_path, text):
-
     img = Image.open(image_path)
 
     os.remove(image_path)
@@ -57,21 +49,17 @@ async def drawText(image_path, text):
     i_width, i_height = img.size
 
     if os.name == "nt":
-
         fnt = "ariel.ttf"
 
     else:
-
         fnt = "./MukeshRobot/resources/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
     if ";" in text:
-
         upper_text, lower_text = text.split(";")
 
     else:
-
         upper_text = text
 
         lower_text = ""
@@ -81,10 +69,9 @@ async def drawText(image_path, text):
     current_h, pad = 10, 5
 
     if upper_text:
-
         for u_text in textwrap.wrap(upper_text, width=15):
-
-            u_width, u_height = draw.textsize(u_text, font=m_font)
+            uwl, uht, uwr, uhb = m_font.getbbox(u_text)
+            u_width, u_height = uwr - uwl, uhb - uht
 
             draw.text(
                 xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
@@ -124,10 +111,9 @@ async def drawText(image_path, text):
             current_h += u_height + pad
 
     if lower_text:
-
         for l_text in textwrap.wrap(lower_text, width=15):
-
-            u_width, u_height = draw.textsize(l_text, font=m_font)
+            uwl, uht, uwr, uhb = m_font.getbbox(l_text)
+            u_width, u_height = uwr - uwl, uhb - uht
 
             draw.text(
                 xy=(
@@ -192,5 +178,4 @@ async def drawText(image_path, text):
 
 __mod_name__ = "ᴍᴍғ"
 __help__ = """ 
-❍ /mmf <ᴛᴇxᴛ> ➛ ᴛᴏ ᴍᴇᴍɪғʏ """
-        
+⫸ /mmf <ᴛᴇxᴛ> ◉ ᴛᴏ ᴍᴇᴍɪғʏ """
