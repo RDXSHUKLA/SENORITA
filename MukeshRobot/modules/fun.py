af34aa049a1143ad26db6d8637b0ee7d1e70342b
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-08-20 05:04:33 +0530

    Update fun.py

--- MukeshRobot/modules/fun.py
+++ MukeshRobot/modules/fun.py
@@ -7,7 +7,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext
 
 import MukeshRobot.modules.fun_strings as fun_strings
-from MukeshRobot import dispatcher,OWNER_ID
+from MukeshRobot import OWNER_ID
 from MukeshRobot.modules.disable import DisableAbleCommandHandler
 from MukeshRobot.modules.helper_funcs.chat_status import is_user_admin
 from MukeshRobot.modules.helper_funcs.extraction import extract_user
