b071dde1a20912621513059753e81400d32719af
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-08-20 05:04:06 +0530

    Update ud.py

--- MukeshRobot/modules/ud.py
+++ MukeshRobot/modules/ud.py
@@ -2,7 +2,7 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 
-from MukeshRobot import dispatcher
+#from MukeshRobot import dispatcher
 from MukeshRobot.modules.disable import DisableAbleCommandHandler
 
 
