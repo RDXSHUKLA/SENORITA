e7c137801b5548f867436ef851c8782d58d59877
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2024-08-20 06:37:23 +0530

    Update couples.py

--- MukeshRobot/modules/couples.py
+++ MukeshRobot/modules/couples.py
@@ -9,7 +9,7 @@ from pyrogram.enums import *
 
 #BOT FILE NAME
 from MukeshRobot import pbot as app
-from MukeshRobot.mongo.couples_db import _get_image, get_couple
+#from MukeshRobot.mongo.couples_db import _get_image, get_couple
 
 def dt():
     now = datetime.now()
